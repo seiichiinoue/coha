0e8c817297977c234b00470e25bcff35d85f6cf3
seiichiinoue <d35inou108@gmail.com>
2023-02-06 19:44:06 +0900

    small change

--- remove_unnecessary_sentences.py
+++ remove_unnecessary_sentences.py
@@ -9,7 +9,7 @@ valid_cnt = 0
 
 with open(input_path, 'r') as input_file, open(output_path, 'w') as output_path:
     for sent in tqdm(input_file.readlines()):
-        if len(sent.split('\t')[1]) < 2:
+        if len(sent.split('\t')[1].strip()) < 2:
             continue
         if "@" in sent:
             continue
